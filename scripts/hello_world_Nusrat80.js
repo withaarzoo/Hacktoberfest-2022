798ff2d17986ac5960a16bbf3feb028c3358771a
Nusrat Jahan <114184034+nusrat80@users.noreply.github.com>
2022-10-01 00:41:47 +0530

    Create hello_world_Nusrat80.js

---
+++ scripts/hello_world_Nusrat80.js
@@ -0,0 +1,11 @@
+// LANGUAGE: C Language
+// ENV: Terminal
+// AUTHOR: Nusrat Jahan
+// GITHUB: https://github.com/nusrat80
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
