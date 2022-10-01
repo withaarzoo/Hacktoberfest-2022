3d709c0ee4e0a0773773df7d328a5be95bace0ed
Rumi Khatun <110076050+RUMI20@users.noreply.github.com>
2022-10-01 07:50:35 +0530

    Create Hello_World_RUMI.C

---
+++ scripts/Hello_World_RUMI.C
@@ -0,0 +1,13 @@
+// LANGUAGE: C language
+// ENV: Terminal
+// AUTHOR: Rumi Khatun
+// GITHUB: https://github.com/RUMI20
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World !");
+
+    return 0;
+}
